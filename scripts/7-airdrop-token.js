3d3c3a57c27f55b2911b9088b0af62a3d184c70e
kohei.yaginuma <f.kohei.yaginuma@gmail.com>
2022-05-08 10:36:17 +0900

    lesson3-1

---
+++ scripts/7-airdrop-token.js
@@ -0,0 +1,47 @@
+import sdk from "./1-initialize-sdk.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+// This is the address to our ERC-1155 membership NFT contract.
+const editionDrop = sdk.getEditionDrop(process.env.INSERT_EDITION_DROP_ADDRESS);
+
+// This is the address of our ERC-20 contract printed out in the step before.
+const token = sdk.getToken(process.env.INSERT_TOKEN_ADDRESS);
+
+(async () => {
+  try {
+    // Grab all the addresses of people who own our membership NFT,
+    // which has a tokenId of 0.
+    const walletAddresses = await editionDrop.history.getAllClaimerAddresses(0);
+
+    if (walletAddresses.length === 0) {
+      console.log(
+        "No NFTs have been claimed yet, maybe get some friends to claim your free NFTs!",
+      );
+      process.exit(0);
+    }
+
+    // Loop through the array of addresses.
+    const airdropTargets = walletAddresses.map((address) => {
+      // Pick a random # between 1000 and 10000.
+      const randomAmount = Math.floor(Math.random() * (50 - 10 + 1) + 10);
+      console.log("✅ Going to airdrop", randomAmount, "tokens to", address);
+
+      // Set up the target.
+      const airdropTarget = {
+        toAddress: address,
+        amount: randomAmount,
+      };
+
+      return airdropTarget;
+    });
+
+    // Call transferBatch on all our airdrop targets.
+    console.log("🌈 Starting airdrop...");
+    await token.transferBatch(airdropTargets);
+    console.log("✅ Successfully airdropped tokens to all the holders of the NFT!");
+  } catch (err) {
+    console.error("Failed to airdrop tokens", err);
+  }
+})();
