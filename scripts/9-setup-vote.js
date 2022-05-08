57733f72db3aba1079171db151a5de460446de91
kohei.yaginuma <f.kohei.yaginuma@gmail.com>
2022-05-08 11:46:05 +0900

    lesson3-3

---
+++ scripts/9-setup-vote.js
@@ -0,0 +1,48 @@
+import sdk from "./1-initialize-sdk.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+// This is our governance contract.
+const vote = sdk.getVote(process.env.INSERT_VOTE_ADDRESS);
+
+// This is the address of our ERC-20 contract printed out in the step before.
+const token = sdk.getToken(process.env.INSERT_TOKEN_ADDRESS);
+
+(async () => {
+  try {
+    // Give our treasury the power to mint additional token if needed.
+    await token.roles.grant("minter", vote.getAddress());
+
+    console.log(
+      "Successfully gave vote contract permissions to act on token contract"
+    );
+  } catch (error) {
+    console.error(
+      "failed to grant vote contract permissions on token contract",
+      error
+    );
+    process.exit(1);
+  }
+
+  try {
+    // Grab our wallet's token balance, remember -- we hold basically the entire supply right now!
+    const ownedTokenBalance = await token.balanceOf(
+      process.env.WALLET_ADDRESS
+    );
+
+    // Grab 90% of the supply that we hold.
+    const ownedAmount = ownedTokenBalance.displayValue;
+    const percent90 = Number(ownedAmount) / 100 * 90;
+
+    // Transfer 90% of the supply to our voting contract.
+    await token.transfer(
+      vote.getAddress(),
+      percent90
+    );
+
+    console.log("✅ Successfully transferred " + percent90 + " tokens to vote contract");
+  } catch (err) {
+    console.error("failed to transfer tokens to vote contract", err);
+  }
+})();
